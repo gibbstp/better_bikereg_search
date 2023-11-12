c1bfa5a280b2e3456208845c8eedd279c89f7358
gibbstp <gibbstp@gmail.com>
2023-11-12 11:20:52 -0600

    move and rewrote file to remove pandas dependency

---
+++ bikereg_search/backend/etl_pipeline/extract/get_events.py
@@ -0,0 +1,97 @@
+import requests, time
+
+
+def api_url_generator(page_number: int) -> str:
+    """
+    Generate BikeReg API URLs based on the page number.
+
+    Arguments:
+    page_number (int): The page number for the API request.
+
+    Returns:
+    str: The generated API URL.
+    """
+    return f"https://www.bikereg.com/api/search?page={page_number}"
+
+
+def request_api(url: str):
+    """
+    Request data from the BikeReg API.
+
+    Arguments:
+    url (str): The URL for the API request.
+
+    Returns:
+    json: BikeReg API response in JSON format.
+    """
+    return requests.get(url).json()
+
+'''
+#TODO: remove this function. Rewrote code to make this repetitive. Leaving until certain. 
+def process_url_response(events_list: list, url_response) -> list:
+    """
+    Process BikeReg API response and extend existing list.
+
+    Arguments:
+    events_list (list): The existing list to which the API response will be added.
+    url_response: BikeReg API response in JSON format.
+
+    Returns:
+    list: The updated list with the new API response events.
+    """
+    
+    # response["MatchingEvents"] is a list of dictionaries
+    return events_list.extend(url_response["MatchingEvents"])
+    '''
+
+
+def bike_reg_api(page_number: int = 0) -> tuple(list[dict], int):
+    """
+    Perform necessary steps to fetch data from the BikeReg API.
+
+    Argunments:
+    page_number (int): The API page number.
+    events_list (pd.DataFrame): The existing DataFrame to which the API response will be added.
+
+    Returns:
+    int: result_count: int
+    """
+    
+    url = api_url_generator(page_number=page_number)
+
+    response = request_api(url=url)
+
+    #TODO: add logging for number of events in response for each page number
+    results_count = response["ResultCount"]
+
+    return response["MatchingEvents"], results_count
+
+def start_api_calls() -> list[dict]:
+    """
+    Perform an iterative loop to fetch data from the BikeReg API until the result count all results are collected.
+
+    Returns:
+    list: The final list containing all the fetched data.
+    """
+    page_number = 0
+    events_list = []
+
+    events_list, results_count = bike_reg_api()
+
+    while results_count == 100:
+        response_events, results_count = bike_reg_api(page_number=page_number)
+        
+        #TODO: add logging for length of list after extend
+        events_list.extend(response_events)
+
+        page_number += 1
+
+        # Bikreg api does not have a robots.txt or a known rate limit
+        # sleep is added here to be nice.
+        time.sleep(0.5)
+
+    return events_list
+    
+
+if __name__ == "__main__":
+    start_api_calls()
